c01978e2f49a3913519ed6a39b2df1a730c3ed47
Helen <howareyouhelen@hotmail.com>
2017-07-20 18:48:59 +0000

    added routes for home page and summary page

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/test_result");
 
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+};
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -43,13 +51,33 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get('/poll_info', (req, res) => {
-  res.json({
-    id: 1,
-    email: 'joel@joel.joel',
-    name: 'joel'
-  })
+app.post("/summary", (req, res) => {
+  const randomPollId = generateRandomString();
+  res.redirect("/summary/${randomPollId}")
 })
+
+// Summary Page
+app.get("/summary/${randomPollId}", (req, res) => {
+  let userPollId = req.params.randomPollId;
+  if (userPollID === undefined) {
+    res.redirect("/")
+  } else {
+    res.render("summary/${randomPollId}")
+  }
+})
+
+// Voting page
+
+
+
+
+// app.get('/poll_info', (req, res) => {
+//   res.json({
+//     id: 1,
+//     email: 'joel@joel.joel',
+//     name: 'joel'
+//   })
+// })
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
