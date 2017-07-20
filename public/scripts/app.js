0d7b1b770d5eb5b51182ae3de97130ce3600105e
Helen <howareyouhelen@hotmail.com>
2017-07-20 03:11:27 +0000

    replaced users to poll_info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,10 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/poll_info"
+  }).done((poll_info) => {
+    for(user of poll_info) {
+      $("<div>").text(poll_info.email).appendTo($("body"));
     }
   });;
 });
