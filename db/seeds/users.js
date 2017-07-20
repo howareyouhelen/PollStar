75c52427524b02f930c6704ab8921c413d6b7ba0
Helen <howareyouhelen@hotmail.com>
2017-07-20 03:10:40 +0000

    changed hard-code to reflect key-values needed for the project

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
+  return knex('poll_info').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('poll_info').insert({email: 'hahahah@example.com', name: 'Alice'}),
+        knex('poll_info').insert({email: 'hahah@example.com', name: 'Bob'}),
+        knex('poll_info').insert({email: 'abcd@example.com', name: 'Charlie'})
       ]);
     });
 };
