a6d4b4f3f34580093b57bc7eff46d8b2d30c951d
Helen <howareyouhelen@hotmail.com>
2017-07-20 03:12:07 +0000

    changed users to test files

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+const usersRoutes = require("./routes/test_result");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -36,12 +36,20 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use("/api/test_result", usersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get('/poll_info', (req, res) => {
+  res.json({
+    id: 1,
+    email: 'joel@joel.joel',
+    name: 'joel'
+  })
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
