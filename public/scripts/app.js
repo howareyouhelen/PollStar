87ca23d5217012a27418cc85b8dd928dfee2e5f4
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-23 19:38:09 +0000

    commented out ajax for now in order to avoid errors form missing hardcoded information

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,15 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/poll_info"
-  }).done((poll_info) => {
-    for(user of poll_info) {
-      $("<div>").text(poll_info.email).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/poll_info"
+//   }).done((poll_info) => {
+//     for(user of poll_info) {
+//       $("<div>").text(poll_info.email).appendTo($("body"));
+//     }
+//   });;
+// });
 
 
-$('#votingpoll').on('submit'{
+// // $('#votingpoll').on('submit'){
 
-}
+// // }
